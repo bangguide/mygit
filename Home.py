28b8730f89efa2bdf8c163f6dd351b1c4a669c58
bangguide <siwakankataythong@gmail.com>
2023-04-08 14:13:24 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 
 #st.header("siwakan")
 st.image("./pic/banner.png")
-st.image("./pic/guide.jpg")
+#st.image("./pic/guide.jpg")
 html_8 = """
 <div style="background-color:#990066;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้</h5></center>
@@ -21,3 +21,20 @@ st.markdown("")
 dt = pd.read_csv("./data/iris.csv")
 
 st.write(dt.head(10))
+
+
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+       st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
