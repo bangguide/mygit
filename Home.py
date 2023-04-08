64c39b56375ecea8e9c34f33dfdd100f94341f5d
bangguide <siwakankataythong@gmail.com>
2023-04-08 13:57:55 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,5 +7,13 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 #st.header("siwakan")
-st.image("./pic/npru.png")
-st.image("./pic/guide.jpg")
+st.image("./mygit/pic/npru.png")
+st.image("./mygit/pic/guide.jpg")
+html_8 = """
+<div style="background-color:#990066;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
