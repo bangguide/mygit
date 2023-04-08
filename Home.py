18e7f429c12e7e24ccb8111f273b595581618b8b
bangguide <siwakankataythong@gmail.com>
2023-04-08 14:15:51 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -33,8 +33,16 @@ dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
 
 
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   st.write(dt.head(10))
+  # st.write(dt.head(10))
    st.bar_chart(dx2)
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
