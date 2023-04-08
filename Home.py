0557d693a465380e6e245f457356625eece285e0
bangguide <siwakankataythong@gmail.com>
2023-04-08 14:21:21 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -65,7 +65,7 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0] == 'Setosa':
-    st.image("./pic/setosa.jpg")
+    st.image("./pic/sentosa.jpg")
    else:       
     st.writ('xxx')    
    #st.button("ไม่แสดงข้อมูล")
