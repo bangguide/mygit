2f84b0719b135846ef9375fbe59d4e38827204c4
bangguide <siwakankataythong@gmail.com>
2023-04-08 14:03:44 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,8 +7,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 #st.header("siwakan")
-st.image("./mygit/pic/npru.png")
-st.image("./mygit/pic/guide.jpg")
+st.image("./pic/npru.png")
+st.image("./pic/guide.jpg")
 html_8 = """
 <div style="background-color:#990066;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้</h5></center>
